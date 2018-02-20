8a3af5dabc1366bf5551a4acfd699c394553401a
mmacneil <markmacneil@gmail.com>
2018-02-19 20:41:51 -0400

    Building out vue frontend.

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,11 +1,11 @@
 <template>  
 <div class="columns">
-  <div class="column is-6" style="background:#f0f0f0">    
-    <div class="content">
+  <div class="column is-10 hello"> 
+   
        <h1 class="title">
         {{ msg }}
-      </h1>      
-    </div>    
+      </h1> 
+       <p>This demo shows local and facebook user registration and login flow using Vue.js v2.5.13, ASP.NET Core 2.0 WebApi and Facebook api</p>        
   </div>
 </div>
 </template>
@@ -18,4 +18,9 @@ export default class HelloWorld extends Vue {
   @Prop() private msg: string;
 }
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello {background:#f0f0f0}
+</style>
  
