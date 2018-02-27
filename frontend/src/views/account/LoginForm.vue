6a1a7ede943c0e727fc3cda16b3cba75454fb358
mmacneil <markmacneil@gmail.com>
2018-02-27 12:36:31 -0400

    Final touches to complete demo.

--- frontend/src/views/account/LoginForm.vue
+++ frontend/src/views/account/LoginForm.vue
@@ -3,8 +3,13 @@
       <div class="container has-text-centered">
         <div class="column is-4 is-offset-4">
           <h3 class="title has-text-grey">Login</h3>
-          <p class="subtitle has-text-grey">Please login to proceed</p>
-          <div class="box">
+          <p class="subtitle has-text-grey">Please login to proceed</p>         
+          <article class="message is-success" v-if="$route.query.new">
+          <div class="message-body">
+            <strong>You're all set {{$route.query.firstName}}!</strong> Login with your password to continue.
+          </div>
+          </article>
+          <div class="box">          
             <form @submit.prevent="handleSubmit">
               <div class="field">
                 <div class="control">
@@ -35,6 +40,7 @@
 import Spinner from '@/components/Spinner.vue'; // @ is an alias to /src
 import { Component, Vue } from 'vue-property-decorator';
 import { Credentials } from '../../models/credentials.interface';
+// this.$route.query.page
 
 @Component({
   components: {
@@ -46,6 +52,12 @@ export default class RegistrationForm extends Vue {
 private isBusy: boolean = false;
 private errors: string = '';
 private credentials = {} as Credentials;
+
+private created() {
+  if (this.$route.query.new) {
+    this.credentials.userName = this.$route.query.email;
+  }
+}
 
 private handleSubmit() {
      this.isBusy = true;
